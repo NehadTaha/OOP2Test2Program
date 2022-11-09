c681289f6b86d8275b20fc8d714ec03e09112c3e
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-09 10:13:23 -0500

    Create class Book

---
+++ OOP2Test2Program/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2Test2Program
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private int year;
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+
+
+        public string Burn()
+        {
+            return author;
+        }
+    }
+
+}
