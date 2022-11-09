d6b7d71188bc36a08b578c53a6c2362ea0cf40f8
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-09 10:13:49 -0500

    Create class text.

---
+++ OOP2Test2Program/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2Test2Program
+{
+    internal class Text
+    {
+        private int length;
+        private string content;
+
+        public int Length { get { return length; } }
+
+        public string Content { get; set; }
+
+
+    }
+}
